35f96760f3501d99134a6f402515c0f625d1fdc6
Lester Jan Artienda <artiele@oocl.com>
2019-10-10 02:48:52 +0800

    Pass tests

--- __tests__/main.spec.js
+++ __tests__/main.spec.js
@@ -1,23 +1,50 @@
 const createMultiplyTable = require('../main');
 
-it ('should display multiple table', () => {
-    const result = "2*2=4\n2*3=6 3*3=9\n2*4=8 3*4=12 4*4=16"
-    console.log(result)
-    expect(createMultiplyTable(2, 4)).toBe(result);
+it('should display multiple table', () => {
+    const expected = '2*2=4\n' + '2*3=6 3*3=9\n' + '2*4=8 3*4=12 4*4=16';
+    expect(createMultiplyTable(2, 4)).toBe(expected);
 });
 
-it ('should return null', () => {
-    expect(createMultiplyTable(4, 2)).toBe(result);
+it('should return null', () => {
+    expect(createMultiplyTable()).toBe(null);
 });
 
-it ('should return null', () => {
-    expect(createMultiplyTable(4, 2)).toBe(result);
+it('should return null', () => {
+    expect(createMultiplyTable(4, 2)).toBe(null);
 });
 
-it ('should return null', () => {
-    expect(createMultiplyTable('a', 2)).toBe(result);
+it('should return null', () => {
+    expect(createMultiplyTable(1, 1001)).toBe(null);
 });
 
-it ('should return null', () => {
-    expect(createMultiplyTable(null, 2)).toBe(result);
+it('should return null', () => {
+    expect(createMultiplyTable('a', 2)).toBe(null);
+});
+
+it('should return null', () => {
+    expect(createMultiplyTable(null, 2)).toBe(null);
+});
+
+it('should return null', () => {
+    expect(createMultiplyTable(1.1, 2)).toBe(null);
+});
+
+it('should return null', () => {
+    expect(createMultiplyTable(1)).toBe(null);
+});
+
+it('should return null', () => {
+    expect(createMultiplyTable(0)).toBe(null);
+});
+
+it('should return null', () => {
+    expect(createMultiplyTable(1001)).toBe(null);
+});
+
+it('should return null', () => {
+    expect(createMultiplyTable(-1, 7)).toBe(null);
+});
+
+it('should return null', () => {
+    expect(createMultiplyTable(-4, -1)).toBe(null);
 });
--- main.js
+++ main.js
@@ -1,55 +1,54 @@
-// module.exports = (num1, num2) => {
-//   return renderTable(num1,num2);
-// };
-
-function createMultiplyTable(num1, num2) {
-    return renderTable(num1,num2);
-};
-
-function isInputValid(num) {
-    return num!==null && Number.isInteger(num) && isValidInteger(num)
+module.exports = (start, end) => {
+    return createMultiplyTable(start, end);
 }
-function isValidInteger(num) {
-    return num > 0 && num <1001;
-}
-function isRangeValid(num1, num2) {
-    return (num2-num1 > -1);
-}
-// function renderTable(num1,num2) {
-//     if (!(isInputValid(num1), isInputValid(num2), isRangeValid(num1, num2))) return null;
-//     let out;
-//     for (let indexOuter = num1; indexOuter <= num2; indexOuter++) {
-//         let column = 1;
-//         "2*2=4\n2*3=6 3*3=9\n2*4=8 3*4=12 4*4=16"
-//         out = `${num1}*${indexOuter}=${num1*indexOuter}`;
-//         column++;
-//         return out;
-//     }
-// }
 
-function renderTable(start, end){
-    let multiplyArray = [];
-    for(; start <= end; start++){
-        let currentNum = start;
-        while(currentNum <= end){
-            multiplyArray.push(start + '*' + currentNum + '=' + start*currentNum);
-            currentNum++;
+function createMultiplyTable(start, end) {
+    if (!isInputValid(start, end)) return null;
+    return formatOutput(start, end);
+}
+function isInputValid(start, end) {
+    return isNumberValid(start) && isNumberValid(end) && isRangeValid(start, end);
+}
+function isNumberValid(num) {
+    return num !== null && Number.isInteger(num) && num > 0 && num < 1001;
+}
+function isRangeValid(start, end) {
+    return (end - start > -1);
+}
+function formatOutput(start, end) {
+    const rowNum = end - start;
+    let currIdx = start;
+    const equationString = generateEquation(start, end);
+    let rowLine = '';
+
+    for (let a = 0; a <= rowNum; a++)
+    {
+        const filteredEquationString = equationString.filter(result => result.multiplier == currIdx);
+        if (filteredEquationString.length == 1)
+        {
+            rowLine += filteredEquationString[0].equation + '\n';
+        } else
+        {
+            filteredEquationString.forEach((v, index) => {
+                rowLine += filteredEquationString[index].equation;
+                (index + 1 == filteredEquationString.length) ? rowLine += '\n' : rowLine += ' ';
+            })
+        }
+        currIdx++;
+    }
+    return rowLine.trim();
+}
+function generateEquation(start, end) {
+    const equationString = [];
+    for (let multiplicand = start; multiplicand <= end; multiplicand++)
+    {
+        for (let multiplier = multiplicand; multiplier <= end; multiplier++)
+        {
+            equationString.push({
+                multiplier,
+                equation: `${multiplicand.toString()}*${multiplier.toString()}=${multiplicand * multiplier}`
+            });
         }
     }
-    console.log(multiplyArray)
-    return multiplyArray;
-};
-
-function arrangeArray(multiplyArray, start,end) {
-    let out = '';
-    for (let indexOuter = 0; indexOuter < ((end-start)+1); indexOuter++) {
-        if (indexOuter === 0) out = multiplyArray[indexOuter]+"\n";
-        for (let indexInner = 0; indexInner <= indexOuter; indexInner++) {
-            out += multiplyArray[indexOuter]+" ";
-        }
-    }
-    console.log(out);
+    return equationString;
 }
-
-const a = [ '2*2=4', '2*3=6', '2*4=8', '3*3=9', '3*4=12', '4*4=16' ];
-arrangeArray(2,4,a);
